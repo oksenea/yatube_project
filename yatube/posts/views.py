7a0e7aae84f0cbfc89456e0ef5298e0b587aa768
Оксана Баяндина <oksenea@my.com>
2022-11-14 15:29:04 +0300

    Add the first urlpatterns

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(reqest):
+    return HttpResponse('Main')
+
+def group_posts(reqest, slug):
+    return HttpResponse(f'String: {slug}')
