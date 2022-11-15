2d67e51c738fe6a3f054524b347b9ea4aa2e7ba9
Оксана Баяндина <oksenea@my.com>
2022-11-16 00:35:00 +0300

    Разбили шаблоны и прописали ссылки

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     # Главная страница
     path('', views.index),
+    path('index.html', views.index),
+    path('group_list.html', views.group_list),
     path('group/<slug:slug>/', views.group_posts)
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Импортируем загрузчик.
+from django.template import loader
 
-# Create your views here.
-def index(reqest):
-    return HttpResponse('Main')
+
+def index(request):
+    # Загружаем шаблон;
+    # шаблоны обычно хранят в отдельной директории.
+    template = 'posts/index.html'
+    # Формируем шаблон
+    return render(request, template)
+
+
+def group_list(request):
+    # Загружаем шаблон;
+    # шаблоны обычно хранят в отдельной директории.
+    template = 'posts/group_list.html'
+    # Формируем шаблон
+    return render(request, template)
+
 
 def group_posts(reqest, slug):
-    return HttpResponse(f'String: {slug}')
+    # Загружаем шаблон;
+    # шаблоны обычно хранят в отдельной директории.
+    template = f'posts/group-{slug}.html'
+    # Формируем шаблон
+    return render(request, template) 
