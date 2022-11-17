74b2c9292e04a00ca49e753d14f930cf8b4820e1
Оксана Баяндина <oksenea@my.com>
2022-11-17 18:38:28 +0300

    Load static

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'post'
+
 urlpatterns = [
     # Главная страница
     path('', views.index),
-    path('index.html', views.index),
-    path('group_list.html', views.group_list),
-    path('group/<slug:slug>/', views.group_posts)
+    path('index.html', views.index, name='main'),
+    path('group_list.html', views.group_list, name='list_group_posts'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts')
 ]
